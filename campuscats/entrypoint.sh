e8b5324233c583860e549bab0d39dcbefc967238
CaptainMorch <captainmorch@gmail.com>
2020-11-15 11:56:20 +0800

    优化 django 启动逻辑
    延长循环 ping 数据库的等待时间；遵循文档建议将 CMD 中命令移入 entrypoint.sh；移除 makemigrations 操作

--- campuscats/entrypoint.sh
+++ campuscats/entrypoint.sh
@@ -3,17 +3,19 @@
 # In case that the database is still initalizing
 echo "Waiting for MySQL..."
 while ! nc -z mysql 3306; do
-    sleep 0.1
+    sleep 1
 done
 echo "MySQL started."
 
 # Update django things
-echo "Making migrations and migrating the database."
-python manage.py makemigrations --noinput
+echo "Try migrating the database."
 python manage.py migrate --noinput
 
 echo "Collecting static files."
 python manage.py collectstatic --noinput
 
+# Starting server
+echo "Strating gunicorn server."
+gunicorn campuscats.wsgi -c /run/secrets/gunicorn.conf.py
 
 exec "$@"
