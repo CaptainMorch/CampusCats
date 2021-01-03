292fe005bfc00bf4ecc6f8f7be57920ea9a6e4af
CaptainMorch <captainmorch@gmail.com>
2021-01-03 21:46:19 +0800

    Fix typo doc->docs

--- settings/__local_settings.py
+++ settings/__local_settings.py
@@ -1,7 +1,7 @@
 # The entrypoint of project settings.
 # 
 # Set your settings here INSTEAD of in the stock file.
-# See https://github.com/CaptainMorch/CampusCats/blob/main/doc/deploy.md#%E4%B8%8B%E8%BD%BD%E5%B9%B6%E8%AE%BE%E7%BD%AE%E9%A1%B9%E7%9B%AE
+# See https://github.com/CaptainMorch/CampusCats/blob/main/docs/deploy.md#%E4%B8%8B%E8%BD%BD%E5%B9%B6%E8%AE%BE%E7%BD%AE%E9%A1%B9%E7%9B%AE
 
 
 # Import django stock settings.
