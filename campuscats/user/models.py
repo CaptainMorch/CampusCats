6525f229723b8d596e2fbe525508951b73855791
CaptainMorch <captainmorch@gmail.com>
2021-01-08 00:12:55 +0800

    Fix that null=True on User.wx_id

--- campuscats/user/models.py
+++ campuscats/user/models.py
@@ -6,7 +6,7 @@ class User(AbstractUser):
     """Site user model. Inherit all from django default user model."""
     phone = models.CharField('电话', blank=True, max_length=11)
     qq = models.PositiveIntegerField('QQ', null=True, blank=True)
-    wx_id = models.CharField('微信ID', max_length=20, null=True, blank=True)
+    wx_id = models.CharField('微信ID', max_length=20, blank=True)
 
     class Meta:
         verbose_name = '用户'
